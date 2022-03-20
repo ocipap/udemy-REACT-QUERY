f9bf9724474d6b751afa278b3648a3bc2c8e15ed
papico <papico@inflab.com>
2022-03-20 14:45:28 +0900

    12. Pre-fetching Data

--- base-blog-em/src/PostDetail.jsx
+++ base-blog-em/src/PostDetail.jsx
@@ -1,3 +1,5 @@
+import {useQuery} from "react-query";
+
 async function fetchComments(postId) {
   const response = await fetch(
     `https://jsonplaceholder.typicode.com/comments?postId=${postId}`
@@ -8,7 +10,7 @@ async function fetchComments(postId) {
 async function deletePost(postId) {
   const response = await fetch(
     `https://jsonplaceholder.typicode.com/postId/${postId}`,
-    { method: "DELETE" }
+    {method: "DELETE"}
   );
   return response.json();
 }
@@ -16,22 +18,23 @@ async function deletePost(postId) {
 async function updatePost(postId) {
   const response = await fetch(
     `https://jsonplaceholder.typicode.com/postId/${postId}`,
-    { method: "PATCH", data: { title: "REACT QUERY FOREVER!!!!" } }
+    {method: "PATCH", data: {title: "REACT QUERY FOREVER!!!!"}}
   );
   return response.json();
 }
 
-export function PostDetail({ post }) {
+export function PostDetail({post}) {
   // replace with useQuery
-  const data = [];
+  const {data} = useQuery(["comments", post.id], () => fetchComments(post.id));
 
   return (
     <>
-      <h3 style={{ color: "blue" }}>{post.title}</h3>
-      <button>Delete</button> <button>Update title</button>
+      <h3 style={{color: "blue"}}>{post.title}</h3>
+      <button>Delete</button>
+      <button>Update title</button>
       <p>{post.body}</p>
       <h4>Comments</h4>
-      {data.map((comment) => (
+      {data?.map((comment) => (
         <li key={comment.id}>
           {comment.email}: {comment.body}
         </li>
--- base-blog-em/src/Posts.jsx
+++ base-blog-em/src/Posts.jsx
@@ -1,26 +1,57 @@
-import { useState } from "react";
+import {useEffect, useState} from "react";
 
-import { PostDetail } from "./PostDetail";
+import {PostDetail} from "./PostDetail";
+import {useQuery, useQueryClient} from "react-query";
+
 const maxPostPage = 10;
 
-async function fetchPosts() {
+async function fetchPosts(pageNum) {
   const response = await fetch(
-    "https://jsonplaceholder.typicode.com/posts?_limit=10&_page=0"
+    `https://jsonplaceholder.typicode.com/posts?_limit=10&_page=${pageNum}`
   );
   return response.json();
 }
 
 export function Posts() {
-  const [currentPage, setCurrentPage] = useState(0);
+  const [currentPage, setCurrentPage] = useState(1);
   const [selectedPost, setSelectedPost] = useState(null);
 
+  const queryClient = useQueryClient();
+
+  useEffect(() => {
+    if (currentPage >= maxPostPage) {
+      return;
+    }
+    const nextPage = currentPage + 1;
+    queryClient.prefetchQuery(["posts", nextPage], () => fetchPosts(nextPage))
+  }, [currentPage, queryClient])
+
   // replace with useQuery
-  const data = [];
+  const {
+    data,
+    isError,
+    error,
+    isLoading,
+    isPreviousData
+  } = useQuery(["posts", currentPage], () => fetchPosts(currentPage), {
+    staleTime: 2000,
+    keepPreviousData: true
+  });
+
+  if (isLoading) return <div>Loading...</div>;
+
+  if (isError) {
+    return (<div>
+      <h2>Error</h2>
+      <p>{error.toString()}</p>
+    </div>);
+  }
 
   return (
     <>
       <ul>
-        {data.map((post) => (
+        이전 데이터? {isPreviousData.toString()}
+        {data?.map((post) => (
           <li
             key={post.id}
             className="post-title"
@@ -31,16 +62,20 @@ export function Posts() {
         ))}
       </ul>
       <div className="pages">
-        <button disabled onClick={() => {}}>
+        <button disabled={currentPage <= 1} onClick={() => {
+          setCurrentPage(prevPage => prevPage - 1);
+        }}>
           Previous page
         </button>
-        <span>Page {currentPage + 1}</span>
-        <button disabled onClick={() => {}}>
+        <span>Page {currentPage}</span>
+        <button disabled={currentPage >= maxPostPage} onClick={() => {
+          setCurrentPage(prevPage => prevPage + 1);
+        }}>
           Next page
         </button>
       </div>
-      <hr />
-      {selectedPost && <PostDetail post={selectedPost} />}
+      <hr/>
+      {selectedPost && <PostDetail post={selectedPost}/>}
     </>
   );
 }
